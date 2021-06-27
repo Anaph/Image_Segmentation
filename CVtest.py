a7fa05df7fa5017d86953ebb602427a23cebed04
Anaph <danilaa001@gmail.com>
2021-06-28 03:24:24 +0700

    Update dataset generator

--- CVtest.py
+++ CVtest.py
@@ -5,92 +5,108 @@ from time import time
 from motion_detection.detector import MotionDetector
 from motion_detection.packer import pack_images
 from numba import jit
+import pandas as pd
+import sys, getopt
 
 
 @jit(nopython=True)
 def filter_fun(b):
     return ((b[2] - b[0]) * (b[3] - b[1])) > 300
 
+def mask(img,polygons):
+    mask = np.zeros_like(img)
+    for color, polygon in polygons:
+        cv2.fillPoly(mask, np.array([polygon], dtype=np.int64), color=color)
+    masked_image = cv2.bitwise_and(img,mask)
+    return masked_image
 
-if __name__ == "__main__":
+def get_argv(argv):
+    inputvideo = ''
+    outputimage = ''
+    outputmask = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:a:p:m:",["ifile=","afile=","pfile=","mfile="])
+    except getopt.GetoptError:
+        print('test.py -i <inputvideo> -a <inputannotations> -p <outputimage> -m <outputmask>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print('test.py -i <inputvideo> -a <inputannotations> -p <outputimage> -m <outputmask>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputvideo = arg
+        elif opt in ("-a", "--afile"):
+            inputannotations = arg
+        elif opt in ("-p", "--pfile"):
+            outputimage = arg
+        elif opt in ("-m", "--mfile"):
+            outputmask = arg
+    print('Input file is "', inputvideo)
+    print('Input annotations is "', inputannotations)
+    print('Output image is "', outputimage)
+    print('Output mask is "', outputmask)
+    return inputvideo, inputannotations, outputimage, outputmask
 
-    # cap = cv2.VideoCapture(0); s
-    cap = cv2.VideoCapture('tmp/video.mov')
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+def process(inputvideo, inputannotations, outputimage, outputmask):
+    cap = cv2.VideoCapture(inputvideo)
 
-    detector = MotionDetector(bg_history=10,
-                              bg_skip_frames=2,
-                              movement_frames_history=2,
-                              brightness_discard_level=5,
-                              bg_subs_scale_percent=0.2,
-                              pixel_compression_ratio=0.1,
-                              group_boxes=True,
-                              expansion_step=5)
+    detector = MotionDetector(bg_history=1,
+                              bg_skip_frames=1,
+                              movement_frames_history=8,
+                              brightness_discard_level=12,
+                              bg_subs_scale_percent=0.5,
+                              pixel_compression_ratio=0.2,
+                              group_boxes=False,
+                              expansion_step=1)
 
-    # group_boxes=True can be used if one wants to get less boxes, which include all overlapping boxes
-
-    b_height = 320
-    b_width = 320
-
-    res = []
-    fc = dict()
     ctr = 0
+    columns = ['Track ID', 'xmin', 'ymin', 'xmax', 'ymax', 'frame', 'lost', 'occluded', 'generated', 'label']
+    labels = ['Biker', 'Pedestrian', 'Skateboarder', 'Cart', 'Car', 'Bus']
+    df = pd.read_csv(inputannotations, delimiter = " ")
+    df.columns = columns
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
         if frame is None:
             break
 
-        begin = time()
 
-        boxes, frame = detector.detect(frame)
-        # boxes hold all boxes around motion parts
+        movement = detector.detect_move(frame)
 
-        ## this code cuts motion areas from initial image and
-        ## fills "bins" of 320x320 with such motion areas.
-        ##
-        results = []
-        if boxes:
-             results, box_map = pack_images(frame=frame, boxes=boxes, width=b_width, height=b_height,
-                                            box_filter=filter_fun)
-            # box_map holds list of mapping between image placement in packed bins and original boxes
-
-        ## end
-
-        for b in boxes:
-            cv2.rectangle(frame, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 1)
-
-        end = time()
-        it = (end - begin) * 1000
-
-        res.append(it)
-        print("StdDev: %.4f" % np.std(res), "Mean: %.4f" % np.mean(res), "Last: %.4f" % it,
-              "Boxes found: ", len(boxes))
-
-        if len(res) > 10000:
-            res = []
-
-        # idx = 0
-        # for r in results:
-        #      idx += 1
-        #      cv2.imshow('packed_frame_%d' % idx, r)
-
+        boxes = df[df.frame==ctr]
+        rect = []
+        color = []
+        for index, row in boxes.iterrows():
+            rect.append([[row['xmin'], row['ymax']],[row['xmax'], row['ymax']], [row['xmax'], row['ymin']], [row['xmin'], row['ymin']]])
+            for i in range(len(labels)):
+                if row['label'] == labels[i]:
+                    color.append(i+1)
+        rect = zip(color, rect)
+        
         ctr += 1
-        nc = len(results)
-        if nc in fc:
-            fc[nc] += 1
-        else:
-            fc[nc] = 0
+        
+        scale_percent = 200 # percent of original size
+        width = int(movement.shape[1] * scale_percent / 100)
+        height = int(movement.shape[0] * scale_percent / 100)
+        dim = (width, height)
 
-        if ctr % 100 == 0:
-            print("Total Frames: ", ctr, "Packed Frames:", fc)
+        kernel = np.ones((3,3),np.uint8)
+        resized = cv2.dilate(movement,kernel,iterations=2)
+        # resize image
+        resized = cv2.resize(resized, dim, interpolation = cv2.INTER_AREA)
+        resized = mask(resized,rect)
+        backtorgb = cv2.cvtColor(resized,cv2.COLOR_GRAY2RGB)
+        # cv2.imshow('diff_frame', backtorgb)
 
-        cv2.imshow('last_frame', frame)
-        cv2.imshow('detect_frame', detector.detection_boxed)
-        cv2.imshow('diff_frame', detector.color_movement)
+        cv2.imwrite(outputimage + str(ctr) + '.png', frame)
+        cv2.imwrite(outputmask + str(ctr) + '.png', backtorgb)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
-    print(fc, ctr)
+
+
+
+if __name__ == "__main__":
+    inputvideo, inputannotations, outputimage, outputmask = get_argv(sys.argv[1:])
+    process(inputvideo, inputannotations, outputimage, outputmask)
